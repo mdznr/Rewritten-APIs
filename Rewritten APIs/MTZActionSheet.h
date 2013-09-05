ee010af0a9b49a7808eb0f2f77da1cee23c2d5ec
Matt Zanchelli <matt@mdznr.com>
2013-09-04 23:53:39 -0400

    Lots of beautiful documentation!

--- Rewritten APIs/MTZActionSheet.h
+++ Rewritten APIs/MTZActionSheet.h
@@ -19,75 +19,120 @@ NS_CLASS_AVAILABLE_IOS(2_0) @interface MTZActionSheet : NSObject
 
 #pragma mark Creating Action Sheets
 
+#warning TODO: what kinds of quick initalization methods should be presented?
+/// Convenience method for initializing an alert view with a particular title.
+/// @return Newly initialized action sheet.
 - (id)init;
+
+/// Convenience method for initalizing an action sheet with a particular title.
+/// @param title The string that appears in the receiver’s title bar.
+/// @return Newly initialized action sheet.
 - (id)initWithTitle:(NSString *)title;
 
 
 #pragma mark Properties
 
+/// The receiver’s delegate or nil if it doesn’t have a delegate.
+/// @discussion When adding buttons with selectors, those selectors are performed on this.
 @property (strong, nonatomic) id<MTZActionSheetDelegate> delegate;
 
-@property (nonatomic) UIActionSheetStyle actionSheetStyle;  // Default is UIActionSheetStyleAutomatic. Ignored if alert is visible
+/// The receiver’s presentation style. Default is UIActionSheetStyleAutomatic.
+/// @discussion This property determines how the action sheet looks when it is presented. For a list of possible values, see the UIActionSheetStyle constants.
+/// @discussion Ignored if action sheet is already visible.
+@property (nonatomic) UIActionSheetStyle style;
 
+/// The string that appears in the receiver’s title bar.
 @property (strong, nonatomic) NSString *title;
 
 #warning should setting visibility show and hide the action sheet? This behaviour works for UIWindow
+/// A Boolean value that indicates whether the receiver is displayed. (read-only)
 @property (nonatomic, readonly, getter=isVisible) BOOL visible;
 
-// Get an ordered array of the button titles
-// Includes cancelButtonTitle and destructiveButtonTitle, if set
+/// An ordered array of the button titles.
+/// Includes cancelButtonTitle and destructiveButtonTitle, if set.
 - (NSArray *)buttonTitles;
 
-// Get an ordered array of the button titles
-// Does not include cancelButtonTitle and destructiveButtonTitle
+/// An ordered array of the button titles.
+/// Does not include cancelButtonTitle and destructiveButtonTitle.
 - (NSArray *)otherButtonTitles;
 
 #warning do we even need this API?
-// Returns the number of buttons
-// Includes the cancel and destructive buttons, if set
+/// The total number of buttons on the action sheet.
+/// Includes cancel and destructive buttons, if set.
 - (NSUInteger)numberOfButtons;
 
 #warning do we even need this API?
-// Returns the number of buttons
-// Does not include cancel and destructive buttons
+/// The number of buttons on the action sheet.
+/// Does not include cancel and destructive buttons.
 - (NSUInteger)numberOfOtherButtons;
 
 
 #pragma mark Configuring Buttons
 
-// The title of the cancel button (stylized and positioned automatically)
-// If nil, the cancel button is not shown.
+/// The title of the cancel button (stylized and positioned automatically).
+/// @discussion If nil, this specialized button is not shown.
 @property (strong, nonatomic) NSString *cancelButtonTitle;
 
-// The title of the destructive button (stylized and positioned automatically)
-// If nil, the destructive button is not shown.
+/// The title of the destructive button (stylized and positioned automatically).
+/// @discussion If nil, this specialized button is not shown.
 @property (strong, nonatomic) NSString *destructiveButtonTitle;
 
-// This will append a button to the action sheet and call the corresponding selector on the delegate when tapped
+/// Append a new button to the action sheet and, when tapped, call the corresponding selector on the delegate
+/// @param title The label that appears on the button
+/// @param selector The selector to be performed on the delegate when the button is tapped
 - (void)addButtonWithTitle:(NSString *)title andSelector:(SEL)selector;
 
-// This will append a button to the action sheet and, when tapped, perform the corresponding block
+/// Append a button to the action sheet and, when tapped, perform the corresponding block
+/// @param title The label that appears on the button
+/// @param selector The block to be performed when the button is tapped
 - (void)addButtonWithTitle:(NSString *)title andBlock:(Block)block;
 
 
 #pragma mark Presenting the Action Sheet
 
-// show a sheet animated. you can specify either a toolbar, a tab bar, a bar butto item or a plain view. We do a special animation if the sheet rises from
-// a toolbar, tab bar or bar button item and we will automatically select the correct style based on the bar style. if not from a bar, we use
-// UIActionSheetStyleDefault if automatic style set
+/// Displays an action sheet that originates from the specified tab bar.
+/// @param view The tab bar from which the action sheet originates.
+/// @discussion The appearance of the action sheet is animated. On iPad, this method centers the action sheet in the middle of the screen. Generally, the showFromRect:inView:animated: method should be used instead when trying to presenting an action sheet relative to a tab bar in an iPad application.
 - (void)showFromTabBar:(UITabBar *)view;
+
+/// Displays an action sheet that originates from the specified toolbar.
+/// @param view The toolbar from which the action sheet originates.
+/// @discussion The appearance of the action sheet is animated. On iPad, this method centers the action sheet in the middle of the screen. Generally, if you want to present an action sheet relative to a toolbar in an iPad application, you should use the showFromBarButtonItem:animated: method instead. Using this method, if style is UIActionSheetStyleAutomatic, UIActionSheetStyleDefault is used.
 - (void)showFromToolbar:(UIToolbar *)view;
+
+/// Displays an action sheet that originates from the specified view.
+/// @param view The view from which the action sheet originates.
+/// @discussion The appearance of the action sheet is animated. On iPad, this method centers the action sheet in the middle of the screen. Generally, if you want to present an action sheet in an iPad application, you should use the showFromRect:inView:animated: method to display the action sheet instead.
+/// @discussion Using this method, if style is UIActionSheetStyleAutomatic, UIActionSheetStyleDefault is used.
 - (void)showInView:(UIView *)view;
-- (void)showFromBarButtonItem:(UIBarButtonItem *)item animated:(BOOL)animated;
-- (void)showFromRect:(CGRect)rect inView:(UIView *)view animated:(BOOL)animated;
+
+/// Displays an action sheet that originates from the specified bar button item.
+/// @param item The bar button item from which the action sheet originates.
+/// @param animated Specify YES to animate the presentation of the action sheet or NO to present it immediately without any animation effects.
+- (void)showFromBarButtonItem:(UIBarButtonItem *)item animated:(BOOL)animated NS_AVAILABLE_IOS(3_2);
+
+/// Displays an action sheet that originates from the specified view.
+/// @param rect
+/// @param view
+/// @param animated
+/// @discussion On iPad, this method displays the action sheet in a popover whose arrow points to the specified rectangle of the view. The popover does not overlap the specified rectangle.
+/// @discussion Using this method, if style is UIActionSheetStyleAutomatic, UIActionSheetStyleDefault is used.
+- (void)showFromRect:(CGRect)rect inView:(UIView *)view animated:(BOOL)animated NS_AVAILABLE_IOS(3_2);
 
 
 #pragma mark Dismissing the Action Sheet
 
-// Hides the alert and behaves like tapping the cancel button
+#warning fix discussion
+/// Dismisses the action sheet, as if the user tapped the cancel button, immediately using an optional animation.
+/// @param animated Specify YES to animate the dismissal of the action sheet or NO to remove the action sheet without an animation.
+/// @discussion You can use this method to dismiss the action sheet programmatically as needed. The action sheet also calls this method itself in response to the user tapping one of the buttons in the action sheet.
+/// @discussion In iOS 4.0, you may want to call this method whenever your application moves to the background. An action sheet is not dismissed automatically when an application moves to the background. This behavior differs from previous versions of the operating system, where they were canceled automatically when the application was terminated. Dismissing the action sheet gives the application a chance to save changes or abort the operation and perform any necessary cleanup in case the application is terminated later.
 - (void)dismissWithCancelAnimated:(BOOL)animated;
 
 // Hides the action sheet. Use this method when you need to explicitly dismiss the action sheet.
+/// Dismisses the action sheet, as if the user tapped the button with the specified title, immediately using an optional animation.
+/// @param buttonTitle The title of the button that was tapped.
+/// @discussion In iOS 4.0, you may want to call this method whenever your application moves to the background. An action sheet is not dismissed automatically when an application moves to the background. This behavior differs from previous versions of the operating system, where they were canceled automatically when the application was terminated. Dismissing the action sheet gives the application a chance to save changes or abort the operation and perform any necessary cleanup in case the application is terminated later.
 - (void)dismissWithTappedButtonTitle:(NSString *)buttonTitle animated:(BOOL)animated;
 
 @end
--- Rewritten APIs/MTZAlertView.h
+++ Rewritten APIs/MTZAlertView.h
@@ -20,71 +20,106 @@ NS_CLASS_AVAILABLE_IOS(2_0) @interface MTZAlertView : NSObject
 #pragma mark Creating Alert Views
 
 #warning TODO: what kinds of quick initalization methods should be presented?
+/// Convenience method for initializing an alert view with a particular title.
+/// @param title The string that appears in the receiver’s title bar.
+/// @return Newly initialized alert view.
 - (id)initWithTitle:(NSString *)title;
+
+/// Convenience method for initializing an alert view with a particular title and message.
+/// @param title The string that appears in the receiver’s title bar.
+/// @param message Descriptive text that provides more details than the title.
+/// @return Newly initialized alert view.
 - (id)initWithTitle:(NSString *)title andMessage:(NSString *)message;
-+ (id)alertViewWithStyle:(UIAlertViewStyle)style;
+
+/// Convenience method for initializing an alert view with a particular style.
+/// @param style The kind of alert displayed to the user.
+/// @return Newly initialized alert view.
++ (id)alertViewWithStyle:(UIAlertViewStyle)style NS_AVAILABLE_IOS(5_0);
 
 
 #pragma mark Properties
 
+/// The receiver’s delegate or nil if it doesn’t have a delegate.
+/// @discussion When adding buttons with selectors, those selectors are performed on this.
 @property (strong, nonatomic) id<MTZAlertViewDelegate> delegate;
 
-@property (nonatomic) UIAlertViewStyle style;
+/// The kind of alert displayed to the user.
+/// @discussion This property determines how the alert view looks when it is presented. For a list of possible values, see the UIAlertViewStyle constants.
+@property (nonatomic) UIAlertViewStyle style NS_AVAILABLE_IOS(5_0);
 
+/// The string that appears in the receiver’s title bar.
 @property (strong, nonatomic) NSString *title;
+
+/// Descriptive text that provides more details than the title.
 @property (strong, nonatomic) NSString *message;
 
 #warning should setting visibility show and hide the alert? This behaviour works for UIWindow
+/// A Boolean value that indicates whether the receiver is displayed. (read-only)
 @property (nonatomic, readonly, getter = isVisible) BOOL visible;
 
-// Get an ordered array of the button titles
-// Includes cancelButtonTitle, if set
+/// An ordered array of the button titles.
+/// Includes cancelButtonTitle, if set.
 - (NSArray *)buttonTitles;
 
-// Get an ordered array of the button titles
-// Does not include cancelButtonTitle
+/// An ordered array of the button titles.
+/// Does not include cancelButtonTitle.
 - (NSArray *)otherButtonTitles;
 
 #warning do we even need this API?
-// Returns the number of buttons
-// Includes the cancel button, if set
+/// The total number of buttons on the alert view.
+/// Includes the cancel button, if set.
 - (NSUInteger)numberOfButtons;
 
 #warning do we even need this API?
-// Returns the number of buttons
-// Does not include cancel button, if set
+/// The number of buttons on the alert view.
+/// Does not include the cancel button.
 - (NSUInteger)numberOfOtherButtons;
 
-// Get the text value for the input fields (only available in some styles)
-- (NSString *)textInInputField;
-
+#warning should have unique methods for each kind of style?
+#warning or should return NSDictionary with field type (e.g. "password") as key
+/// The text value for the input fields (only available in some styles)
+/// @discussion UIAlertViewStyleDefault will have no input fields and will always return nil
+/// @discussion UIAlertViewStylePlainTextInput will have one input field
+/// @discussion UIAlertViewStyleSecureTextInput will have one input field
+/// @discussion UIAlertViewStyleLoginAndPasswordInput will have two input fields.
+- (NSString *)textInInputField NS_AVAILABLE_IOS(5_0);
 
 
 #pragma mark Configuring Buttons
 
-// The title of the cancel button (stylized and positioned automatically)
-// If nil, the button is not shown.
+/// The title of the cancel button (stylized and positioned automatically).
+/// @discussion If nil, this specialized button is not shown.
 @property (strong, nonatomic) NSString *cancelButtonTitle;
 
-// This will append a button to the alert and call the corresponding selector on the delegate when tapped
+/// Append a new button to the alert and, when tapped, call the corresponding selector on the delegate
+/// @param title The label that appears on the button
+/// @param selector The selector to be performed on the delegate when the button is tapped
 - (void)addButtonWithTitle:(NSString *)title andSelector:(SEL)selector;
 
-// This will append a button to the alert and, when tapped, perform the corresponding block
+/// Append a button to the alert and, when tapped, perform the corresponding block
+/// @param title The label that appears on the button
+/// @param selector The block to be performed when the button is tapped
 - (void)addButtonWithTitle:(NSString *)title andBlock:(Block)block;
 
 
 #pragma mark Presenting the Alert View
 
-// Shows the popup alert animated
+/// Displays the receiver using animation.
 - (void)show;
 
 
 #pragma mark Dismissing the Alert View
 
-// Hides the alert and behaves like tapping the cancel button
+/// Dismisses the receiver as if the user tapped the cancel button, optionally with animation.
+/// This does not work if cancelButtonTitle is not set.
+/// @param animated YES if the receiver should be removed by animating it first; otherwise, NO if it should be removed immediately with no animation.
+/// @discussion In iOS 4.0, you may want to call this method whenever your application moves to the background. An alert view is not dismissed automatically when an application moves to the background. This behavior differs from previous versions of the operating system, where they were canceled automatically when the application was terminated. Dismissing the alert view gives your application a chance to save changes or abort the operation and perform any necessary cleanup in case your application is terminated later.
 - (void)dismissWithCancelAnimated:(BOOL)animated;
 
-// Hides the alert. Use this method when you need to explicitly dismiss the alert.
+/// Dismisses the receiver, optionally with animation. This should only be used when explicity dismissing the alert is absolutely necessary.
+/// @param buttonTitle The title of the button that was tapped just before invoking this method.
+/// @param animated YES if the receiver should be removed by animating it first; otherwise, NO if it should be removed immediately with no animation.
+/// @discussion In iOS 4.0, you may want to call this method whenever your application moves to the background. An alert view is not dismissed automatically when an application moves to the background. This behavior differs from previous versions of the operating system, where they were canceled automatically when the application was terminated. Dismissing the alert view gives your application a chance to save changes or abort the operation and perform any necessary cleanup in case your application is terminated later.
 - (void)dismissWithTappedButtonTitle:(NSString *)buttonTitle animated:(BOOL)animated;
 
 
@@ -98,29 +133,41 @@ NS_CLASS_AVAILABLE_IOS(2_0) @interface MTZAlertView : NSObject
 
 #pragma mark Responding to Actions
 
-// Called when the cancel button is tapped. The alert will be automatically dismissed after this call returns
+/// Sent to the delegate when the user taps the cancel button on an alert view.
+/// @param alertView The alert view containing the button.
+/// @discussion The receiver is automatically dismissed after this method is invoked.
 - (void)alertViewDidTapCancelButton:(MTZAlertView *)alertView;
 
 
 #pragma mark Customizing Behavior
 
-// Called before animation and showing alert
+/// Sent to the delegate before an alert view is presented to the user.
+/// @param alertView The alert view that is about to be displayed.
+/// @discussion Called before animation and showing alert
 - (void)willPresentAlertView:(MTZAlertView *)alertView;
 
-// Called after animation
+/// Sent to the delegate after an alert view is presented to the user.
+/// @param alertView The alert view that was displayed.
+/// @discussion Called after animation of showing alert
 - (void)didPresentAlertView:(MTZAlertView *)alertView;
 
 #warning rework the way this works
 // Called after edits in any of the default fields added by the style
 //- (BOOL)alertViewShouldEnableFirstOtherButton:(UIAlertView *)alertView;
 #warning should be more like this? (Called for each (non-cancel?) button
+/// Sent to the delegate to determine whether a button (with a certain title) on the alert should be enabled.
+/// @param alertView The alert view that is being configured.
+/// @param buttonTitle The title of the button that should or shouldn't be enabled.
+/// @return YES if the button should be enabled, NO if the button should be disabled.
 - (BOOL)alertView:(MTZAlertView *)alertView shouldEnableButtonWithTitle:(NSString *)buttonTitle;
 
 
 #pragma mark Canceling
 
-// Called when we cancel a view (eg. the user clicks the Home button). This is not called when the user clicks the cancel button.
-// If not defined in the delegate, we simulate a click in the cancel button
+#warning @discussion is incorrect. Did cancel and will cancel?
+/// Sent to the delegate when an alert view has been canceled.
+/// @param alertView The alert view that did get canceled.
+/// @discussion This is sent to the delegate when an alert is canceled (e.g. the user clicks the Home button). This is not called when the user clicks the cancel button. If the alert view’s delegate does not implement this method, clicking the cancel button is simulated and the alert view is dismissed. Implement this method if some actions need to be performed before an alert view is canceled. An alert view can be canceled at any time by the system—for example, when the user taps the Home button. The alertView:willDismissWithButtonIndex: and alertView:didDismissWithButtonIndex: methods are invoked after this method.
 - (void)alertViewDidCancel:(MTZAlertView *)alertView;
 
 
